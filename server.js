6409a899e7914396b4649cbac24e06cfef582aad
CharlyesRodrigues <charlyessouza@gmail.com>
2022-10-22 20:21:29 -0300

    Atualizado

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 
 import express  from "express";
+import {v4 as uuidv4} from "uuid";
 
 //Criando um servidor com o nome app
 const app = express(); 
+app.use(express.json());
 /* CRUD =>
 C - CREATE - CRIAR
 R - READ   - LER
@@ -20,23 +22,34 @@ res.status(200).json({todos});
 
 app.post ("/todos", (req,res )=> {
 const { body }   =  req;
+const todo = body;
 
-todos.push(body);
+todo.concluded = false;
+todo.id = uuidv4();
+//body.date = moment(date).format("DD/MM/YYYY") ;
+todo.date = new Date();
 
-res.status(201).send()
+
+
+todos.push(todo);
+
+res.status(201).send(todo )
     
     
 })
 app.put("/todos", (req,res )=> {
 
+
+
+    
     res.status(202).send()
     
     
 })
     
 app.delete("/todos", (req,res )=> {
-    
-       
+    const { body }   =  req;
+    todos.delete(body);
     res.status(202).send()
         
 })

d5f240fa13d9a491a533069f18b40db5baae428a
CharlyesRodrigues <charlyessouza@gmail.com>
2022-10-22 18:58:11 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,45 @@
+
 import express  from "express";
 
 //Criando um servidor com o nome app
 const app = express(); 
+/* CRUD =>
+C - CREATE - CRIAR
+R - READ   - LER
+U - UPDATE - ATUALIZAR
+D - DELETE - DELETAR   */
 
-app.get("/", (req,res )=> {
+var todos =[];
+app.get("/todos", (req,res )=> {
 
-res.status(200).json({message: "Hello Word"});
+    //Busca de todos
+res.status(200).json({todos});
 
 
 })
+
+app.post ("/todos", (req,res )=> {
+const { body }   =  req;
+
+todos.push(body);
+
+res.status(201).send()
+    
+    
+})
+app.put("/todos", (req,res )=> {
+
+    res.status(202).send()
+    
+    
+})
+    
+app.delete("/todos", (req,res )=> {
+    
+       
+    res.status(202).send()
+        
+})
+    
 
 app.listen(3333, () => console.log("Server running in localhost: 3333")); 

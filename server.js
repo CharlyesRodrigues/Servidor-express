20eda8859d5dd3bd05e300575daf7288f8e127b6
CharlyesRodrigues <charlyessouza@gmail.com>
2022-10-22 18:23:42 -0300

    Servidor-Express

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-import { Express } from "express";
+import express  from "express";
 
-const app = express();
+//Criando um servidor com o nome app
+const app = express(); 
 
 app.get("/", (req,res )=> {
 
-res.status(200).json({message: "Hello word"});
+res.status(200).json({message: "Hello Word"});
 
 
 })
 
-app.listen(3333, () => console.log("Server running in localhost: 3333"))
+app.listen(3333, () => console.log("Server running in localhost: 3333")); 
